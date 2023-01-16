3699fd73ef1e4c340673ad0d19b696ebf2269c8c
ShankarJava <122591708+ShankarJava@users.noreply.github.com>
2023-01-16 19:53:27 +0530

    Update Demo.java
    Topull

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 class Demo{
 public static void main(String[] args)
 {
-System.out.println("WELCOME TO GIT CONSOLEDemo");
+System.out.println("WELCOME TO GIT CONSOLE");
+  System.out.println("Changes made in hub");
 }
 }
