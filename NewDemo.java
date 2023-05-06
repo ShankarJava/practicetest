fb20bd9fa6a3b6e25c0a26b1c669937776f13286
ShankarTogari <ShankarThogarii@gmail.com>
2023-05-06 23:48:37 +0530

    Update

--- NewDemo.java
+++ NewDemo.java
@@ -2,5 +2,8 @@ class NewDemo{
 public static void main(String[] args){
 
 System.out.println("May 6 2023 23:42");
+System.out.println("May 6 2023 23:44");
+System.out.println("May 6 2023 23:46");
+
 }
 }
